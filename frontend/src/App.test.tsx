3be7c8944728ff7c840dc043218d1e59f80b18bd
Sarah Feairheller <feairheller.3@buckeyemail.osu.edu>
2026-04-11 14:24:50 -0400

    changes

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -3,8 +3,13 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('The main app', () => {
+  it('should render the Orders link', () => {
+    render(<App />);
+    expect(screen.getByText('Orders')).toBeInTheDocument();
+  });
+  it('should render the Payments link', () => {
+    render(<App />);
+    expect(screen.getByText('Payments')).toBeInTheDocument();
+  })
 });
