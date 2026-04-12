9cd1cf5734e184f1a9b58951984fd595738a281f
Ryan Blessing <ryan.blessing6@gmail.com>
2026-04-12 13:27:38 -0400

    Add unit tests for Home link and Home header

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -10,6 +10,14 @@ describe('The main app', () => {
   });
   it('should render the Payments link', () => {
     render(<App />);
-    expect(screen.getByText('Payments')).toBeInTheDocument();
+    expect(screen.getByText('Payment')).toBeInTheDocument();
+  });
+  it('should render the Home link', () => {
+    render(<App />);
+    expect(screen.getByRole('link', { name: "Home" })).toBeInTheDocument();
+  });
+  it('should render the Home header', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
   });
 });
